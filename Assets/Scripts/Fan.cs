278de649703d1a642adeb37240c0e219100f790a
nobody14641 <addiekann@gmail.com>
2018-02-09 14:31:47 -0800

    This is different

---
+++ Assets/Scripts/Fan.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour 
+{
+	public float forward;
+
+	public Rigidbody rb;
+	// Use this for initialization
+	void Awake () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter (Collider trig)
+	{
+		if (trig.gameObject.tag == "fan") {
+			GetComponent<Rigidbody> ().velocity = Vector3.forward * forward;
+		}	
+	}
+}
