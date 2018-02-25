d2d16147d091dd92324977ccbd0a5855a6192070
nobody14641 <addiekann@gmail.com>
2018-02-25 14:38:58 -0800

    I am still working on this

--- Assets/Scripts/FanRotation.cs
+++ Assets/Scripts/FanRotation.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FanRotation : MonoBehaviour {
-
+public class FanRotation : MonoBehaviour 
+{
+	public Transform from;
+	public Transform to;
+	public float speed;
+	public bool rotation = false;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		if (rotation == true) {
+			transform.rotation = Quaternion.Slerp (from.rotation, to.rotation, Time.time * speed);
+			rotation = false;
+		}
 		
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.tag == "ball")
+			{
+				rotation = true;
+			}
 	}
 }
