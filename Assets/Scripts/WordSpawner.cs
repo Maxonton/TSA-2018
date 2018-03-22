c08b4a14268547e464603814874851cfd451ab2f
Maxonton <elijahgreisz@outlook.com>
2018-03-22 00:38:12 -0700

    sgfds

--- Assets/Scripts/WordSpawner.cs
+++ Assets/Scripts/WordSpawner.cs
@@ -6,7 +6,6 @@ public class WordSpawner : MonoBehaviour {
 
     public GameObject wordPrefab;
     public Transform wordCanvas;
-	//public Transform[] SpawnPoint;
 
     public float ycoord =700f;
 
@@ -18,11 +17,7 @@ public class WordSpawner : MonoBehaviour {
         float xcoord = Random.Range(minx, maxx);
         Vector3 topPosition = new Vector3(xcoord,ycoord);
 
-<<<<<<< HEAD
-		GameObject wordObj = Instantiate(wordPrefab, randomPosition, Quaternion.identity, wordCanvas);
-=======
         GameObject wordObj = Instantiate(wordPrefab, topPosition, Quaternion.identity, wordCanvas);
->>>>>>> 5521d808b93100be0a82297eb9e120c414a5055d
         WordDisplay wordDisplay = wordObj.GetComponent<WordDisplay>();
 
         return wordDisplay;
