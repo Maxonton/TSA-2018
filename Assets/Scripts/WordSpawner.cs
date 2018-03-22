bdf3bbad15ea0dd36fd8eb19a59148671953f144
nobody14641 <addiekann@gmail.com>
2018-03-22 00:16:19 -0700

    I am sorry I am useless.

--- Assets/Scripts/WordSpawner.cs
+++ Assets/Scripts/WordSpawner.cs
@@ -6,12 +6,13 @@ public class WordSpawner : MonoBehaviour {
 
     public GameObject wordPrefab;
     public Transform wordCanvas;
+	//public Transform[] SpawnPoint;
 
 	public WordDisplay SpawnWord()
     {
         Vector3 randomPosition = new Vector3(Random.Range(-2.5f,2.5f),7f);
 
-        GameObject wordObj = Instantiate(wordPrefab, randomPosition, Quaternion.identity, wordCanvas);
+		GameObject wordObj = Instantiate(wordPrefab, randomPosition, Quaternion.identity, wordCanvas);
         WordDisplay wordDisplay = wordObj.GetComponent<WordDisplay>();
 
         return wordDisplay;
